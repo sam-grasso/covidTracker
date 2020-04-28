b9509ee6396953981e798e4dad870dce36765278
Anshul Mishra <anshulm.dev@gmail.com>
2020-04-28 15:40:21 +0530

    Add files via upload

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -25,73 +25,80 @@ function number_format(number, decimals, dec_point, thousands_sep) {
         s[1] += new Array(prec - s[1].length + 1).join('0');
     }
     return s.join(dec);
-}
-
-// Area Chart Example
-$(document).ready(function(){
-  $.getJSON('https://api.covid19india.org/data.json', function(jd) {
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Jan 31", "Feb 01", "Feb 02", "Feb 09", "Feb 16", "Feb 23", "Mar 08", "Mar 15", "Mar 22", "Mar 29", "Apr 05", "Apr 12", "Apr 19"],
-        datasets: [{
-            label: "Spread",
-            lineTension: 0.3,
-            backgroundColor: "rgba(78, 115, 223, 0.05)",
-            borderColor: "rgba(78, 115, 223, 1)",
-            pointRadius: 3,
-            pointBackgroundColor: "rgba(78, 115, 223, 1)",
-            pointBorderColor: "rgba(78, 115, 223, 1)",
-            pointHoverRadius: 3,
-            pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-            pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-            pointHitRadius: 10,
-            pointBorderWidth: 2,
-            data: [jd.cases_time_series[1].totalconfirmed, jd.cases_time_series[2].totalconfirmed, jd.cases_time_series[3].totalconfirmed, jd.cases_time_series[11].totalconfirmed, jd.cases_time_series[18].totalconfirmed, jd.cases_time_series[25].totalconfirmed, jd.cases_time_series[39].totalconfirmed, jd.cases_time_series[46].totalconfirmed, jd.cases_time_series[53].totalconfirmed, jd.cases_time_series[60].totalconfirmed, jd.cases_time_series[67].totalconfirmed, jd.cases_time_series[74].totalconfirmed,jd.cases_time_series[81].totalconfirmed],
-        }],
-    },
-    options: {
-        maintainAspectRatio: false,
-        layout: {
-            padding: {
-                left: 10,
-                right: 25,
-                top: 25,
-                bottom: 0
-            }
-        },
-        scales: {
-            xAxes: [{
-                time: {
-                    unit: 'date'
-                },
-                gridLines: {
-                    display: false,
-                    drawBorder: false
-                },
-                ticks: {
-                    maxTicksLimit: 7
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    maxTicksLimit: 5,
-                    padding: 10,
-                },
-                gridLines: {
-                    color: "rgb(234, 236, 244)",
-                    zeroLineColor: "rgb(234, 236, 244)",
-                    drawBorder: false,
-                    borderDash: [2],
-                    zeroLineBorderDash: [2]
-                }
-            }],
-        },
-        legend: {
-            display: false
+} // Area Chart Example
+$(document).ready(function() {
+    $.getJSON('https://api.covid19india.org/data.json', function(jd) {
+        var ctx = document.getElementById("myAreaChart");
+        let degree = parseInt(jd.cases_time_series.length / 20)
+        let labelex = []
+        let dataex = []
+        for (let i = 0; i < 21; i++) {
+            labelex.push(`"${jd.cases_time_series[degree*i].date}"`)
+            dataex.push(jd.cases_time_series[degree * i].totalconfirmed)
         }
-    }
+        labelex.push(`"${jd.cases_time_series[jd.cases_time_series.length-1].date}"`)
+        dataex.push(jd.cases_time_series[jd.cases_time_series.length - 1].totalconfirmed)
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labelex,
+                datasets: [{
+                    label: "Spread",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(78, 115, 223, 0.05)",
+                    borderColor: "rgba(78, 115, 223, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: dataex,
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        time: {
+                            unit: 'date'
+                        },
+                        gridLines: {
+                            display: false,
+                            drawBorder: false
+                        },
+                        ticks: {
+                            maxTicksLimit: 7
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            maxTicksLimit: 5,
+                            padding: 10,
+                        },
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                legend: {
+                    display: false
+                }
+            }
+        });
     });
-});
 });
